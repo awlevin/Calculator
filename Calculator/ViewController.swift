a0b2c2369795f1b03c35eff41bee8c7fafdd1f2b
aaron <awlevin@comcast.net>
2016-01-11 13:19:38 -0600

    First Commit

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,16 +10,98 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var display: UILabel!
+    
+    var userIsInTheMiddleOfTypingANumber = false
+
+    @IBAction func appendDigit(sender: UIButton) {
+        let digit = sender.currentTitle!
+        if(userIsInTheMiddleOfTypingANumber) {
+            if (!display.text!.containsString(".") || digit.compare(".").rawValue != 0) {
+                display.text = display.text! + digit
+            }
+        } else{
+            userIsInTheMiddleOfTypingANumber = true
+            
+            if (digit.compare(".").rawValue == 0) {
+                display.text = "0\(digit)"
+            }
+            else {
+                display.text = digit
+            }
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func operate(sender: UIButton) {
+        let operation = sender.currentTitle!
+        if userIsInTheMiddleOfTypingANumber {
+            enter()
+        }
+        print(operation)
+        switch operation {
+        case "✕": performOperation {$0 * $1}
+        case "÷": performOperation {$1 / $0}
+        case "+": performOperation {$0 + $1}
+        case "−": performOperation {$1 - $0}
+        case "√": performOperation { sqrt($0) }
+        case "sin": performOperation { sin($0) }
+        case "cos": performOperation { cos($0) }
+        case "C":
+            operandStack.removeAll()
+            displayValue = 0
+        default: break
+        }
     }
-
-
+    @IBAction func piButton(sender: UIButton) {
+        if userIsInTheMiddleOfTypingANumber {
+            enter()
+        }
+        displayValue = M_PI
+        enter()
+    }
+    
+    @IBAction func deleteButton(sender: UIButton) {
+        if (display.text!.characters.count > 1) {
+            display.text = String(display.text!.characters.dropLast())
+        } else {
+            displayValue = 0
+            userIsInTheMiddleOfTypingANumber = false
+        }
+    }
+    
+    
+    func performOperation(operation: (Double, Double) -> Double) {
+        if (operandStack.count >= 2) {
+            displayValue = operation(operandStack.removeLast(), operandStack.removeLast())
+            enter()
+        }
+    }
+    
+    private func performOperation(operation: Double -> Double) {
+        if (operandStack.count >= 1) {
+            displayValue = operation(operandStack.removeLast())
+            enter()
+        }
+    }
+    
+    var operandStack = Array<Double>()
+    
+    @IBAction func enter() {
+        userIsInTheMiddleOfTypingANumber = false
+        operandStack.append(displayValue)
+        print("op stack = \(operandStack)")
+    }
+    
+    var displayValue: Double {
+        get{
+            return NSNumberFormatter().numberFromString(display.text!)!.doubleValue
+        }
+        set{
+            display.text = (newValue == 0) ? "0" : "\(newValue)"
+            
+            userIsInTheMiddleOfTypingANumber = false
+        }
+    }
+    
 }
 
